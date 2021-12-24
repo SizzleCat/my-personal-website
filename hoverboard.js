8a6040f1afc871715f8be7221a86765e01262622
SizzleCat <smaraghy@gmail.com>
2021-12-24 01:08:58 -0800

    testing to see if 5000 squares (vs 10000) improves site performance

--- hoverboard.js
+++ hoverboard.js
@@ -1,27 +1,7 @@
 const hovContainer = document.getElementById("hoverboard-container");
-// const colors = [
-//   "#f55919",
-//   "#68e17f",
-//   "#c0cbff",
-//   "#fff",
-//   "#ff1efe",
-//   "#b1ff00",
-//   "#00d2ff",
-//   "#ff3a67",
-// ];
-// const colors = [
-//   "red",
-//   "#ff4500",
-//   "yellow",
-//   "#66ff00",
-//   "cyan",
-//   "magenta",
-//   "#0096ff",
-//   "#ee4b2b",
-// ];
-// const colors = ["#3e74d1", "#00cbef", "#39e0ef", "#78ed86", "#f2ff66"];
+
 const colors = ["#84f4ff", "#9effaa", "#31f484", "#0ff1ff", "#2672ec"];
-const SQUARES = 10000;
+const SQUARES = 5000;
 
 for (let i = 0; i < SQUARES; i++) {
   const square = document.createElement("div");
